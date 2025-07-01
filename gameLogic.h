38f4c3b0c4f86ce237ff477667d5598fef124db1
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-01 20:30:21 +0700

    add gameLogic.h

---
+++ gameLogic.h
@@ -0,0 +1,25 @@
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+usiung namespace std;
+
+// Forward declaration of Piece class
+class Piece;
+
+// Global game state variables (defined in gameLogic.cpp)
+extern vector<Piece*> board;  
+extern vector<int> highlightSquares; 
+extern bool isWhiteTurn;
+extern vector<Piece*> capturedWhitePieces; 
+extern vector<Piece*> capturedBlackPieces; 
+
+// Function declarations
+void initializeBoard();
+int convertPosToIndex(const string& pos);
+void executeMove(int fromIndex, int toIndex);
+
+#endif // GAME_LOGIC_H
