6c4e791e0faaf290cde005e556e8c50d53635c8a
Hantaro171902 <chaugiahan001@gmail.com>
2025-07-01 20:12:30 +0700

    add board.h

--- board.h
+++ board.h
@@ -14,4 +14,15 @@ extern string blackBg;
 extern string greenBg;  
 extern string reset;     
 
-// 
+// Forward declaration of global variables used in board display
+extern vector<class Piece*> board;  
+extern vector<int> highlightSquares; 
+extern vector<class Piece*> capturedWhitePieces;
+extern vector<class Piece*> capturedBlackPieces;
+
+// Function declarations
+void clearTerminalScreen();
+void printBoard();
+bool isSquareHighlighted(int index);
+
+#endif // BOARD_DISPLAY_H
